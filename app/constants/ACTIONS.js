df15c52da8bd218b84b493eee09b7d7cbea7ed53
Yaroslav Zaklinskyi <yaroslav0507@gmail.com>
2016-05-25 17:54:29 +0300

    gh-pages

--- app/constants/ACTIONS.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-export default {
-    SET_FACE: 'SET_FACE',
-    SET_HAIR: 'SET_HAIR',
-    SET_GLASSES: 'SET_GLASSES',
-    SET_CLOTHES: 'SET_CLOTHES'
-}
--- webpack.config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-global.webpack = require('webpack');
-global.path = require('path');
-global.projectDir = __dirname;
-require('dotenv').load();
-
-module.exports = require('./webpack/config');
